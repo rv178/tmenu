8ef06108b4ba39d2182d41ce954e7f5a329bc6f7
rv178 <idliyout@gmail.com>
2022-07-09 14:10:33 +0530

    [update] switch to crossterm backend

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::io;
-use termion::raw::IntoRawMode;
-use tui::backend::TermionBackend;
-use tui::widgets::{Block, Borders};
+use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
 fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout().into_raw_mode()?;
-    let backend = TermionBackend::new(stdout);
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Tmenu").borders(Borders::ALL);
-        f.render_widget(block, size);
-    })
+    Ok(())
 }
