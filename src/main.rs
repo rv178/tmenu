51cc5f261be1088f4219e930e24cc320054e0a23
rv178 <idliyout@gmail.com>
2022-07-10 11:39:57 +0530

    [rm] input mode

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,12 @@ use tui::{
 };
 use unicode_width::UnicodeWidthStr;
 
-enum InputMode {
-    Normal,
-    Editing,
-}
-
 /// Tmenu holds the state of the application
 struct Tmenu {
     /// Current value of the input box
     input: String,
     /// Current input mode
-    input_mode: InputMode,
     /// History of recorded messages
-    search_string: String,
     app_list: Vec<AppItem>,
     index: usize,
 }
@@ -43,8 +36,6 @@ impl Tmenu {
     fn default() -> Tmenu {
         Tmenu {
             input: String::new(),
-            input_mode: InputMode::Normal,
-            search_string: String::new(),
             app_list: Vec::new(),
             index: 0,
         }
@@ -112,61 +103,37 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: Tmenu) -> io::Result
         terminal.draw(|f| ui(f, &app))?;
 
         if let Event::Key(key) = event::read()? {
-            match app.input_mode {
-                InputMode::Normal => match key.code {
-                    KeyCode::Char('i') => {
-                        app.input_mode = InputMode::Editing;
-                    }
-                    KeyCode::Char('q') => {
-                        return Ok(());
-                    }
-                    KeyCode::Char('k') => {
-                        app.previous();
-                    }
-                    KeyCode::Char('j') => {
-                        app.next();
-                    }
-                    KeyCode::Enter => {
-                        match Command::new("sh")
-                            .arg("-c")
-                            .arg(app.app_list[app.index].cmd.to_string())
-                            .stdin(Stdio::null())
-                            .stdout(Stdio::null())
-                            .stderr(Stdio::null())
-                            .output()
-                        {
-                            Ok(_) => {}
-                            Err(e) => {
-                                println!("Failed to execute command. Error: `{}`", e);
-                            }
+            match key.code {
+                KeyCode::Enter => {
+                    match Command::new("sh")
+                        .arg("-c")
+                        .arg(app.app_list[app.index].cmd.to_string())
+                        .stdin(Stdio::null())
+                        .stdout(Stdio::null())
+                        .stderr(Stdio::null())
+                        .output()
+                    {
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("Failed to execute command. Error: `{}`", e);
                         }
+                    }
 
-                        exit(0);
-                    }
-                    KeyCode::Up => {
-                        app.previous();
-                    }
-                    KeyCode::Down => {
-                        app.next();
-                    }
-                    _ => {}
-                },
-                InputMode::Editing => match key.code {
-                    KeyCode::Enter => {
-                        app.search_string.clear();
-                        app.search_string.push_str(&app.input);
-                    }
-                    KeyCode::Char(c) => {
-                        app.input.push(c);
-                    }
-                    KeyCode::Backspace => {
-                        app.input.pop();
-                    }
-                    KeyCode::Esc => {
-                        app.input_mode = InputMode::Normal;
-                    }
-                    _ => {}
-                },
+                    exit(0);
+                }
+                KeyCode::Up => {
+                    app.previous();
+                }
+                KeyCode::Down => {
+                    app.next();
+                }
+                KeyCode::Char(c) => {
+                    app.input.push(c);
+                }
+                KeyCode::Backspace => {
+                    app.input.pop();
+                }
+                _ => {}
             }
         }
     }
@@ -186,82 +153,28 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &Tmenu) {
         )
         .split(f.size());
 
-    let (msg, style) = match app.input_mode {
-        InputMode::Normal => (
-            vec![
-                Span::raw("Press "),
-                Span::styled(
-                    "q",
-                    Style::default()
-                        .add_modifier(Modifier::BOLD)
-                        .fg(Color::Blue),
-                ),
-                Span::raw(" to exit, "),
-                Span::styled(
-                    "i",
-                    Style::default()
-                        .add_modifier(Modifier::BOLD)
-                        .fg(Color::Blue),
-                ),
-                Span::raw(" to search, "),
-                Span::styled(
-                    "up/down",
-                    Style::default()
-                        .add_modifier(Modifier::BOLD)
-                        .fg(Color::Blue),
-                ),
-                Span::raw(" or "),
-                Span::styled(
-                    "j/k",
-                    Style::default()
-                        .add_modifier(Modifier::BOLD)
-                        .fg(Color::Blue),
-                ),
-                Span::raw(" to navigate."),
-            ],
-            Style::default().add_modifier(Modifier::BOLD),
-        ),
-        InputMode::Editing => (
-            vec![
-                Span::raw("Press "),
-                Span::styled(
-                    "Esc",
-                    Style::default()
-                        .add_modifier(Modifier::BOLD)
-                        .fg(Color::Blue),
-                ),
-                Span::raw(" to stop searching."),
-            ],
-            Style::default(),
-        ),
-    };
+    let (msg, style) = (
+        vec![
+            Span::raw("Press "),
+            Span::styled(
+                "Up/Down key",
+                Style::default()
+                    .add_modifier(Modifier::BOLD)
+                    .fg(Color::Blue),
+            ),
+            Span::raw(" to navigate. "),
+        ],
+        Style::default().add_modifier(Modifier::BOLD),
+    );
     let mut text = Text::from(Spans::from(msg));
     text.patch_style(style);
     let help_message = Paragraph::new(text);
     f.render_widget(help_message, chunks[0]);
 
     let input = Paragraph::new(app.input.as_ref())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
         .block(Block::default().borders(Borders::ALL).title("Input"));
     f.render_widget(input, chunks[1]);
-    match app.input_mode {
-        InputMode::Normal =>
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            {}
-
-        InputMode::Editing => {
-            // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
-            f.set_cursor(
-                // Put cursor past the end of the input text
-                chunks[1].x + app.input.width() as u16 + 1,
-                // Move one line down, from the border to the input line
-                chunks[1].y + 1,
-            )
-        }
-    }
+    f.set_cursor(chunks[1].x + app.input.width() as u16 + 1, chunks[1].y + 1);
 
     let app_list: Vec<ListItem> = app
         .app_list
